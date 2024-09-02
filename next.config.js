9af1e092757934974da9b70d79862513c63c790e
FLOW <l-zh22@mails.tsinghua.edu.cn>
2024-09-02 22:28:19 +0800

    Update coonfig

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
     output: "standalone",
     reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
     swcMinify: true,
-
-    async rewrites() {
-        return [{
-            source: "/api/:path*",
-            // Change to your backend URL in production
-            destination: process.env.NODE_ENV === 'release' ? "https://django-1ttake37hree.app.secoder.net/:path*" : (process.env.NODE_ENV === 'development' ? "https://django-dev-1ttake37hree.app.secoder.net/:path*" : "http://127.0.0.1:8000/:path*") ,
-        }];
-    }
 };
 
 // eslint-disable-next-line no-undef
